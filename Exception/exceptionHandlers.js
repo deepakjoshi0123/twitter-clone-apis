7f5764c2b44c7935ee30d183e6450a54683f5a82
deepak.j <deepak.j@media.net>
2023-10-26 12:26:42 +0530

    error template updated

--- Exception/exceptionHandlers.js
+++ Exception/exceptionHandlers.js
@@ -4,14 +4,14 @@ const mailer = require("../Mail/config");
 module.exports = {
   TypeError: (error) => {
     logger.error("A TypeError occurred:", error);
-    new mailer().sendEmail({ ...error, timestamp: new Date().toISOString() });
+    new mailer().sendEmail(error);
   },
   ReferenceError: (error) => {
     logger.error("A ReferenceError occurred:", error);
-    new mailer().sendEmail({ ...error, timestamp: new Date().toISOString() });
+    new mailer().sendEmail(error);
   },
   default: (error) => {
     logger.error("Exception occurred:", error);
-    new mailer().sendEmail({ ...error, timestamp: new Date().toISOString() });
+    new mailer().sendEmail(error);
   },
 };
